b0c227c3026aef066d6ca42dba41ad46ce22a469
328295484@qq.com <wht@123456>
2019-07-17 13:57:25 +0800

    自己写的demo

--- openfeign/src/main/java/com/wht/openfeign/api/ApiService.java
+++ openfeign/src/main/java/com/wht/openfeign/api/ApiService.java
@@ -4,6 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+//@FeignClient
 @FeignClient(value = "eurekaclient")
 public interface ApiService {
 
